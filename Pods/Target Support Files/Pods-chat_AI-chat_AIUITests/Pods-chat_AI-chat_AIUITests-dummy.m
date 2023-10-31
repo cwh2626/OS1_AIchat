3daf7ff64cd9d64715eee70cd9376c3ea5c6c8ec
cwh2626 <cwh2626@gmail.com>
2023-10-31 17:00:49 +0900

    Remove Pods directory from repository

--- Pods/Target Support Files/Pods-chat_AI-chat_AIUITests/Pods-chat_AI-chat_AIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_chat_AI_chat_AIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_chat_AI_chat_AIUITests
-@end
--- Pods/Target Support Files/Pods-chat_AITests/Pods-chat_AITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_chat_AITests : NSObject
-@end
-@implementation PodsDummy_Pods_chat_AITests
-@end
